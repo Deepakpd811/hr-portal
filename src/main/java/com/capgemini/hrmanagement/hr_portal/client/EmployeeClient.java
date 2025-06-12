521c2713f1dd081a96641cd1e79abe290ea139c1
Rishav1167 <Rishav1167>
2025-06-12 19:10:13 +0530

    [Rishav] feat: implemented frontend integration with backend employee API to support listing, editing, and adding employee records

---
+++ src/main/java/com/capgemini/hrmanagement/hr_portal/client/EmployeeClient.java
@@ -0,0 +1,30 @@
+package com.capgemini.hrmanagement.hr_portal.client;
+
+
+import com.capgemini.hrmanagement.hr_portal.dto.ApiResponseDtowithoutpageSingleData;
+import com.capgemini.hrmanagement.hr_portal.dto.EmployeeDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+
+@FeignClient(name = "employeeClient", url = "${backend.url}")
+public interface EmployeeClient {
+
+    @GetMapping("/api/employees")
+    ApiResponseDtowithoutpageSingleData<?> getAllEmployees(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size
+    );
+
+    @GetMapping("/api/employees/{id}")
+    ApiResponseDtowithoutpageSingleData getEmployeeById(@PathVariable("id") BigDecimal id);
+
+    @PutMapping("/api/employees/{id}")
+    ApiResponseDtowithoutpageSingleData updateEmployee(@PathVariable("id") BigDecimal id, @RequestBody EmployeeDTO employeeDTO);
+
+    @PostMapping("/api/employees")
+    ApiResponseDtowithoutpageSingleData createEmployee(@RequestBody EmployeeDTO dto);
+
+
+}
