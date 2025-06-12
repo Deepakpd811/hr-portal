edaae138622df281b264af376cc105095f8e92de
himaanshii <himaaanshiii30@gmail.com>
2025-06-12 22:20:04 +0530

    [Himanshi] add. Adding frontend for job table

---
+++ src/main/java/com/capgemini/hrmanagement/hr_portal/dto/EmployeeJobDTO.java
@@ -0,0 +1,21 @@
+package com.capgemini.hrmanagement.hr_portal.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class EmployeeJobDTO {
+
+        private BigDecimal employeeId;
+        private String firstName;
+        private String lastName;
+        private BigDecimal salary;
+        private String email;
+    }
+
